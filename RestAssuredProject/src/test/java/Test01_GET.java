1d043a2e0526c8acb41a1477d0dff93ba2bcbce3
suresh <psuresh2501@gmail.com>
2020-12-19 01:09:30 +0530

    Fist Commit

---
+++ RestAssuredProject/src/test/java/Test01_GET.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Test01_GET {
+
+	@Test
+	void test_01() {
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+		
+		System.out.println(response.getStatusCode());
+		//System.out.println(response.getBody());
+
+	}
+}
